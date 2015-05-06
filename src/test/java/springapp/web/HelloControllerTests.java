d26b1cc6b3abe61841a12ada0875ff6770e545cd
harri feng <harrifeng@gmail.com>
2015-05-06 17:55:36 +0800

    pass the gradlew test

---
+++ src/test/java/springapp/web/HelloControllerTests.java
@@ -0,0 +1,20 @@
+package springapp.web;
+
+
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import springapp.web.HelloController;
+
+import static org.junit.Assert.*;
+
+
+public class HelloControllerTests {
+
+    @Test
+    public void testHandleRequestView() throws Exception{
+        HelloController controller = new HelloController();
+        ModelAndView modelAndView = controller.handleRequest(null, null);
+        assertEquals("hello.jsp", modelAndView.getViewName());
+    }
+}
