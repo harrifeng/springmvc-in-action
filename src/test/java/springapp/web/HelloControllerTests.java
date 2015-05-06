4cc4bbd50f3f5ab6c8261f2c03cece82dd167f85
harri feng <harrifeng@gmail.com>
2015-05-06 20:09:20 +0800

    Sample 3 works Open => http://localhost:8080/springapp/hello.htm

--- src/test/java/springapp/web/HelloControllerTests.java
+++ src/test/java/springapp/web/HelloControllerTests.java
@@ -15,6 +15,9 @@ public class HelloControllerTests {
     public void testHandleRequestView() throws Exception{
         HelloController controller = new HelloController();
         ModelAndView modelAndView = controller.handleRequest(null, null);
-        assertEquals("hello.jsp", modelAndView.getViewName());
+        assertEquals("WEB-INF/jsp/hello.jsp", modelAndView.getViewName());
+        assertNotNull(modelAndView.getModel());
+        String nowValue = (String) modelAndView.getModel().get("now");
+        assertNotNull(nowValue);
     }
 }
